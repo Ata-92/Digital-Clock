2cbdad10ae86ba48302586f4ffc16c9b630e13fe
Ata-92 <atakal92@gmail.com>
2021-05-10 21:47:16 +0300

    Access all elements

---
+++ app.js
@@ -0,0 +1,17 @@
+const day = document.getElementById("day");
+
+const month = document.getElementById("month");
+
+const year = document.getElementById("year");
+
+const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].sort();
+const weekday = document.getElementById("weekday");
+
+const hour = document.getElementById("hour");
+
+const minute = document.getElementById("minute");
+
+const second = document.getElementById("second");
+
+const noon = document.getElementById("noon");
+
